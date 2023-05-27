abfec5c7c24939027837d1186109e76fdbf73f5a
Elyzavetka <lizamybella@gmail.com>
2023-05-27 23:18:42 +0100

    Update closure.js

--- closure.js
+++ closure.js
@@ -7,17 +7,33 @@
 // const calc = createCalcFunction(42);
 // calc();
 
-function createIncrementor(n) {
-  return function (num) {
-    return n + num;
+// function createIncrementor(n) {
+//   return function (num) {
+//     return n + num;
+//   };
+// }
+
+// const addOne = createIncrementor(1);
+// const addTen = createIncrementor(10);
+
+// console.log(addOne(10));
+// console.log(addOne(41));
+
+// console.log(addTen(10));
+// console.log(addTen(41));
+
+function bind(context, fn) {
+  return function (...args) {
+    fn.apply(context, args);
   };
 }
 
-const addOne = createIncrementor(1);
-const addTen = createIncrementor(10);
+function logPerson() {
+  console.log(`Person:  ${this.name}, ${this.age}, ${this.job}`);
+}
 
-console.log(addOne(10));
-console.log(addOne(41));
+const person1 = { name: "Peter", age: 35, job: "FrontEnd" };
+const person2 = { name: "Lora", age: 25, job: "SMM" };
 
-console.log(addTen(10));
-console.log(addTen(41));
+bind(person1, logPerson)();
+bind(person2, logPerson)();
